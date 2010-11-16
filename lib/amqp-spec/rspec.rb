575a082e34788acf99fa62943438a7e0166688f0
arvicco <arvitallian@gmail.com>
2010-11-16 10:02:31 +0300

    New specs still not passing for sync and em_after

--- lib/amqp-spec/rspec.rb
+++ lib/amqp-spec/rspec.rb
@@ -108,175 +108,206 @@ module AMQP
       end
     end
 
-
-    # Yields to given block inside EM.run and AMQP.start loops. This method takes any option that is
-    # also accepted by EventMachine::connect. Also, options for AMQP.start include:
-    # * :user => String (default ‘guest’) - The username as defined by the AMQP server.
-    # * :pass => String (default ‘guest’) - The password for the associated :user as defined by the AMQP server.
-    # * :vhost => String (default ’/’)    - The virtual host as defined by the AMQP server.
-    # * :timeout => Numeric (default nil) - *Connection* timeout, measured in seconds.
-    # * :logging => true | false (default false) - Toggle the extremely verbose AMQP logging.
-    #
-    # In addition to EM and AMQP options, :spec_timeout option (in seconds) is used to force spec to timeout
-    # if something goes wrong and EM/AMQP loop hangs for some reason. SpecTimeoutExceededError is raised.
-    #
     def amqp opts={}, &block
       opts = self.class.default_options.merge opts
       spec_timeout  = opts.delete(:spec_timeout) || self.class.default_timeout
-      @_em_spec_with_amqp = true
-      begin
-        run_em_spec_fiber spec_timeout, opts, &block
-      rescue Exception => outer_spec_exception
-        # Make sure AMQP state is cleaned even after Rspec failures
-#        puts "In amqp, caught '#{outer_spec_exception}', @_em_spec_exception: '#{@_em_spec_exception}'"
-        AMQP.cleanup_state
-        raise outer_spec_exception
-      end
+      EventedLoop.amqp opts, &block
     end
 
-    # Yields to block inside EM loop, :spec_timeout option (in seconds) is used to force spec to timeout
-    # if something goes wrong and EM/AMQP loop hangs for some reason. SpecTimeoutExceededError is raised.
-    #
     def em(spec_timeout = self.class.default_timeout, &block)
       spec_timeout = spec_timeout[:spec_timeout] || self.class.default_timeout if spec_timeout.is_a?(Hash)
-      @_em_spec_with_amqp = false
-      run_em_spec_fiber spec_timeout, &block
+      EventedLoop.em spec_timeout, &block
     end
 
-    # Sets timeout for current running example
-    #
+    def done(delay=nil, &block)
+      EventedLoop.done delay, &block
+    end
+
     def timeout(spec_timeout)
-      EM.cancel_timer(@_em_timer) if @_em_timer
-      @_em_timer = EM.add_timer(spec_timeout) do
-        @_em_spec_exception = SpecTimeoutExceededError.new
-        done
-      end
+      EventedLoop.timeout spec_timeout
     end
 
-    # Breaks the event loop and finishes the spec. This should be called after
-    # you are reasonably sure that your expectations either succeeded or failed.
-    # Done yields to any given block first, then stops EM event loop.
-    # For amqp specs, stops AMQP and cleans up AMQP state.
-    #
-    # You may pass delay (in seconds) to done. If you do so, please keep in mind
-    # that your (default or explicit) spec timeout may fire before your delayed done
-    # callback is due, leading to SpecTimeoutExceededError
-    #
-    def done(delay=nil)
-      done_proc = proc do
-        yield if block_given?
-        EM.next_tick do
-          if @_em_spec_with_amqp
-            if AMQP.conn and not AMQP.closing
-              AMQP.stop_connection do
+    # Represents any type of spec supposed to run inside event loop
+    class EventedLoop
+
+      # Yields to given block inside EM.run and AMQP.start loops. This method takes any option that is
+      # also accepted by EventMachine::connect. Also, options for AMQP.start include:
+      # * :user => String (default ‘guest’) - The username as defined by the AMQP server.
+      # * :pass => String (default ‘guest’) - The password for the associated :user as defined by the AMQP server.
+      # * :vhost => String (default ’/’)    - The virtual host as defined by the AMQP server.
+      # * :timeout => Numeric (default nil) - *Connection* timeout, measured in seconds.
+      # * :logging => true | false (default false) - Toggle the extremely verbose AMQP logging.
+      #
+      # In addition to EM and AMQP options, :spec_timeout option (in seconds) is used to force spec to timeout
+      # if something goes wrong and EM/AMQP loop hangs for some reason. SpecTimeoutExceededError is raised.
+      #
+      def amqp opts={}, &block
+        @_em_spec_with_amqp = true
+        begin
+          run_em_spec_fiber spec_timeout, opts, &block
+        rescue Exception => outer_spec_exception
+          # Make sure AMQP state is cleaned even after Rspec failures
+#        puts "In amqp, caught '#{outer_spec_exception}', @_em_spec_exception: '#{@_em_spec_exception}'"
+          AMQP.cleanup_state
+          raise outer_spec_exception
+        end
+      end
+
+      # Yields to block inside EM loop, :spec_timeout option (in seconds) is used to force spec to timeout
+      # if something goes wrong and EM/AMQP loop hangs for some reason. SpecTimeoutExceededError is raised.
+      #
+      def em(spec_timeout = self.class.default_timeout, &block)
+        spec_timeout = spec_timeout[:spec_timeout] || self.class.default_timeout if spec_timeout.is_a?(Hash)
+        @_em_spec_with_amqp = false
+        run_em_spec_fiber spec_timeout, &block
+      end
+
+      # Sets timeout for current running example
+      #
+      def timeout(spec_timeout)
+        EM.cancel_timer(@_em_timer) if @_em_timer
+        @_em_timer = EM.add_timer(spec_timeout) do
+          @_em_spec_exception = SpecTimeoutExceededError.new
+          done
+        end
+      end
+
+      # Breaks the event loop and finishes the spec. This should be called after
+      # you are reasonably sure that your expectations either succeeded or failed.
+      # Done yields to any given block first, then stops EM event loop.
+      # For amqp specs, stops AMQP and cleans up AMQP state.
+      #
+      # You may pass delay (in seconds) to done. If you do so, please keep in mind
+      # that your (default or explicit) spec timeout may fire before your delayed done
+      # callback is due, leading to SpecTimeoutExceededError
+      #
+      def done(delay=nil)
+        done_proc = proc do
+          yield if block_given?
+          EM.next_tick do
+            if @_em_spec_with_amqp
+              if AMQP.conn and not AMQP.closing
+                AMQP.stop_connection do
+                  finish_em_spec_fiber { AMQP.cleanup_state }
+                end
+              else
                 finish_em_spec_fiber { AMQP.cleanup_state }
               end
             else
-              finish_em_spec_fiber { AMQP.cleanup_state }
+              finish_em_spec_fiber
             end
-          else
-            finish_em_spec_fiber
           end
         end
-      end
-      if delay
-        EM.add_timer delay, &done_proc
-      else
-        done_proc.call
-      end
-    end
-
-    # Retrieves metadata passed in from enclosing example groups
-    #
-    def metadata
-      @metadata ||= self.class.metadata.dup rescue {}
-    end
-
-    # Wraps async method with a callback into a synchronous method call
-    # that returns only after callback is finished (or exception raised)
-    #
-    # TODO: should we add exception processing here?
-    # TODO: what do we do in case if errback fires instead of callback?
-    # TODO: it may happen that callback is never called, and no exception raised either...
-    #
-    def sync *args, &callback
-      args, callable = callable_from *args, &callback
-      fiber = Fiber.current
-      callable.call(*args) do |*returns|
-        fiber.resume callback.call(*returns)
+        if delay
+          EM.add_timer delay, &done_proc
+        else
+          done_proc.call
+        end
       end
 
-      Fiber.yield
-    end
+      # Retrieves metadata passed in from enclosing example groups
+      #
+      def metadata
+        @metadata ||= self.class.metadata.dup rescue {}
+      end
 
-    alias synchronize sync
+      # Wraps async method with a callback into a synchronous method call
+      # that returns only after callback is finished (or exception raised)
+      #
+      # TODO: should we add exception processing here?
+      # TODO: what do we do in case if errback fires instead of callback?
+      # TODO: it may happen that callback is never called, and no exception raised either...
+      #
+      def sync *args, &callback
+        args, callable = callable_from *args, &callback
+        fiber = Fiber.current
+        callable.call(*args) do |*returns|
+          fiber.resume callback.call(*returns)
+        end
 
-    private
+        Fiber.yield
+      end
 
-    # Used to extract async callable from given arguments
-    def callable_from *args, &callback
-      raise ArgumentError, 'Sync method expects callback block' unless callback
-      callable = case args.first
-                   when Method, Proc
-                     args.shift
-                   when nil
-                     raise ArgumentError, 'Sync method expects async callable (possibly with args)'
-                   when Symbol, String
-                     method_name = args.shift
-                     raise ArgumentError, "Wrong method name #{method_name}" unless respond_to? method_name
-                     method(method_name)
-                   else
-                     object = args.shift
-                     method_name = args.shift
-                     raise ArgumentError, "Wrong method name #{method_name}" unless object.respond_to? method_name
-                     object.method(method_name)
-                 end
-      [args, callable]
-    end
+      alias synchronize sync
 
-    # Stops EM loop, executes optional block, finishes off fiber and raises exception if any
-    #
-    def finish_em_spec_fiber
-      self.class.em_hooks[:after][:each].reverse.each { |hook| instance_eval_with_rescue(&hook) }
-      EM.stop_event_loop if EM.reactor_running?
-      yield if block_given?
-      @_em_spec_fiber.resume if @_em_spec_fiber.alive?
-      raise @_em_spec_exception if @_em_spec_exception
-    end
+      private
 
-    # Runs given block inside separate EM event-loop fiber
-    #
-    # TODO: difference between #em and #amqp is in following: in line 221,
-    # TODO: block is EXECUTED for #em, but only added as callback for #amqp.
-    # TODO: therefore, fiber ends for amqp before block's execution even started
-    #
-    # TODO: probably, this can be corrected by introducing another fiber,
-    # TODO: this time wrapping AMQP.start async action... #syncronize, anyone?
-    #
-    def run_em_spec_fiber spec_timeout, opts = {}, &block
-      EM.run do
-        # Running em_before hooks
-        self.class.em_hooks[:before][:each].each { |hook| instance_eval(&hook) }
+      # Used to extract async callable from given arguments
+      def callable_from *args, &callback
+        raise ArgumentError, 'Sync method expects callback block' unless callback
+        callable = case args.first
+                     when Method, Proc
+                       args.shift
+                     when nil
+                       raise ArgumentError, 'Sync method expects async callable (possibly with args)'
+                     when Symbol, String
+                       method_name = args.shift
+                       raise ArgumentError, "Wrong method name #{method_name}" unless respond_to? method_name
+                       method(method_name)
+                     else
+                       object = args.shift
+                       method_name = args.shift
+                       raise ArgumentError, "Wrong method name #{method_name}" unless object.respond_to? method_name
+                       object.method(method_name)
+                   end
+        [args, callable]
+      end
 
-        @_em_spec_exception = nil
-        timeout(spec_timeout) if spec_timeout
-        @_em_spec_fiber = Fiber.new do
-          begin
-            if @_em_spec_with_amqp
-              sync AMQP.method(:start_connection), opts, &block
-            else
-              block.call
-            end
-          rescue Exception => @_em_spec_exception
+      # Stops EM loop, executes optional block, finishes off fiber and raises exception if any
+      #
+      def finish_em_spec_fiber
+        self.class.em_hooks[:after][:each].reverse.each { |hook| instance_eval_with_rescue(&hook) }
+        EM.stop_event_loop if EM.reactor_running?
+        yield if block_given?
+        @_em_spec_fiber.resume if @_em_spec_fiber.alive?
+        raise @_em_spec_exception if @_em_spec_exception
+      end
+
+      # Runs given block inside separate EM event-loop fiber
+      #
+      # TODO: difference between #em and #amqp is in following: in line 221,
+      # TODO: block is EXECUTED for #em, but only added as callback for #amqp.
+      # TODO: therefore, fiber ends for amqp before block's execution even started
+      #
+      # TODO: probably, this can be corrected by introducing another fiber,
+      # TODO: this time wrapping AMQP.start async action... #syncronize, anyone?
+      #
+      def run_em_spec_fiber spec_timeout, opts = {}, &block
+        EM.run do
+          # Running em_before hooks
+          self.class.em_hooks[:before][:each].each { |hook| instance_eval(&hook) }
+
+          @_em_spec_exception = nil
+          timeout(spec_timeout) if spec_timeout
+          @_em_spec_fiber = Fiber.new do
+            begin
+              if @_em_spec_with_amqp
+                sync AMQP.method(:start_connection), opts, &block
+              else
+                block.call
+              end
+            rescue Exception => @_em_spec_exception
 #            puts "In inner run_em_spec_fiber, caught '#{@_em_spec_exception}'"
-            done
+              done
+            end
+            Fiber.yield
           end
-          Fiber.yield
+          @_em_spec_fiber.resume
         end
-        @_em_spec_fiber.resume
       end
+
+    end # class EventedLoop
+
+    # Represents spec running inside AMQP.run loop
+    class EMLoop < EventedLoop
+
     end
-  end
+
+# Represents spec running inside AMQP.run loop
+    class AMQPLoop < EventedLoop
+
+    end
+  end # module SpecHelper
 
 # If you include AMQP::Spec module into your example group, each example of this group will run
 # inside AMQP.start loop without the need to explicitly call 'amqp'. In order to provide options
